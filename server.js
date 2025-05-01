f7f8f682c0774290e13678be9a1b5d9dcd14a7e6
Prem Yadav <prem05082002@gmail.com>
2025-05-01 13:58:06 +0530

    database connection and slide api created

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-const express = require ('express');
-const cors  = require('cors')
 require('dotenv').config();
+const express = require ('express');
+const cors  = require('cors');
+const dotenv = require('dotenv');
+const pool = require('./config/db');
+
+dotenv.config();
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -9,10 +14,27 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//Routes
+app.get('/', async (req, res) => {
+    try {
+      const result = await pool.query('SELECT NOW()');
+      res.send(`Connected to PostgreSQL. Server time: ${result.rows[0].now}`);
+    } catch (err) {
+      res.status(500).send('Database error');
+    }
+  });
 
-app.get('/', (req, res) => {
-    res.send("Backend is  runnig")
-})
+  // Test route to get data from hero_slides
+app.get('/api/hero-slides', async (req, res) => {
+    try {
+      const result = await pool.query('SELECT * FROM hero_slides');
+      res.json(result.rows); // send rows as JSON response
+    } catch (err) {
+      console.error('Error fetching hero_slides:', err);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  });
+
 
 
 app.listen(PORT, () => {
